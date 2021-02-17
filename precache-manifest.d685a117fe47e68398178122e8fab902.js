86192ee28dd7040f367f0329d89eff1f4b535b49
Nicolas Tabountchikoff <ntabountchikoff+gitlab@protonmail.com>
2021-02-17 16:27:52 +0100

    Update portfolio

---
+++ precache-manifest.d685a117fe47e68398178122e8fab902.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6143d2094950b969b2cdeba0c6f55100",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a4e499496d649bd0c5f",
+    "url": "/static/css/main.12ce57ee.chunk.css"
+  },
+  {
+    "revision": "0a5b319457f812cbc929",
+    "url": "/static/js/2.33b34f7b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.33b34f7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a4e499496d649bd0c5f",
+    "url": "/static/js/main.fa8a22fe.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "77eee7bf10b9b932e6686d51a2c39e38",
+    "url": "/static/media/ft.77eee7bf.ttf"
+  },
+  {
+    "revision": "9fefccfba7f61278916ae295dde62afa",
+    "url": "/static/media/ft.9fefccfb.woff2"
+  },
+  {
+    "revision": "a242fa8e67551b08e0632d3d11ca2bce",
+    "url": "/static/media/ft.a242fa8e.woff"
+  },
+  {
+    "revision": "e6c6294168721b5ddd238be409ae2b1c",
+    "url": "/static/media/ft.e6c62941.svg"
+  },
+  {
+    "revision": "f10c9d649f3a004a93df52e77eba8d8f",
+    "url": "/static/media/ft.f10c9d64.eot"
+  },
+  {
+    "revision": "7c78536e008df32b52b03e5da09a79e6",
+    "url": "/static/media/penguin.7c78536e.svg"
+  }
+]);
